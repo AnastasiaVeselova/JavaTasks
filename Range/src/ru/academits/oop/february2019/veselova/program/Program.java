224aaabb99ef853f211234422bd61daac95e3162
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-03-07 17:58:03 +0700

    Added corrections to the Range* task.

--- Range/src/ru/academits/oop/february2019/veselova/program/Program.java
+++ Range/src/ru/academits/oop/february2019/veselova/program/Program.java
@@ -1,4 +1,6 @@
-package ru.academits.oop.february2019.veselova;
+package ru.academits.oop.february2019.veselova.program;
+
+import ru.academits.oop.february2019.veselova.range.Range;
 
 public class Program {
     public static void main(String[] args) {
@@ -10,6 +12,10 @@ public class Program {
 
         Range e = new Range(14, 18);
         Range f = new Range(10, 22);
+
+        Range g = new Range(a);
+        Range h = new Range(16, 20);
+        Range i = new Range(12, 14);
 
         Range intersectionAB = a.computeIntersection(b);
         print("intersection", a, b, intersectionAB);
@@ -35,18 +41,25 @@ public class Program {
         print("complement", a, e, complementAE);
         Range[] complementAF = a.computeComplement(f);
         print("complement", a, f, complementAF);
+        Range[] complementAG = a.computeComplement(g);
+        print("complement", a, g, complementAG);
+        Range[] complementAH = a.computeComplement(h);
+        print("complement", a, h, complementAH);
+        Range[] complementAI = a.computeComplement(i);
+        print("complement", a, i, complementAI);
     }
 
     private static void print(String keyWord, Range first, Range second, Range... result) {
         System.out.printf("%s of %s and %s:\t", keyWord, first.toString(), second.toString());
-        if (result == null) {
-            System.out.print("empty");
+        if (result.length == 0) {
+            System.out.print("null");
         } else {
             for (Range el : result
             ) {
                 System.out.printf("%s\t", el);
             }
         }
+
         System.out.println();
         System.out.println();
     }
--- Range/src/ru/academits/oop/february2019/veselova/range/Range.java
+++ Range/src/ru/academits/oop/february2019/veselova/range/Range.java
@@ -1,4 +1,4 @@
-package ru.academits.oop.february2019.veselova;
+package ru.academits.oop.february2019.veselova.range;
 
 public class Range {
     private double from;
@@ -9,12 +9,16 @@ public class Range {
         this.to = to;
     }
 
+    public Range(Range other) {
+        this(other.getFrom(), other.getTo());
+    }
+
     public double getLength() {
         return to - from;
     }
 
     public boolean isInside(double value) {
-        return (from < value && value < to);
+        return (from <= value && value <= to);
     }
 
     public Range computeIntersection(Range other) {
@@ -27,31 +31,30 @@ public class Range {
 
     public Range[] computeUnion(Range other) {
         if (isIntersected(other)) {
-            return new Range[]{new Range(Math.min(from, other.from), Math.max(to, other.to))
-            };
+            return new Range[]{new Range(Math.min(from, other.from), Math.max(to, other.to))};
         }
 
-        return new Range[]{this, other};
+        return new Range[]{new Range(this), new Range(other)};
     }
 
     public Range[] computeComplement(Range other) {
         if (isIntersected(other)) {
             if (from < other.from) {
-                if (to < other.to) {
-                    return new Range[]{new Range(from, other.from)};
+                if (to > other.to) {
+                    return new Range[]{new Range(from, other.from), new Range(other.to, to)};
                 } else {
-                    return new Range[]{new Range(from, other.from), new Range(other.to, from)};
+                    return new Range[]{new Range(from, other.from)};
                 }
             } else {
                 if (to > other.to) {
                     return new Range[]{new Range(other.to, to)};
                 } else {
-                    return null;
+                    return new Range[]{};
                 }
             }
         }
 
-        return new Range[]{this};
+        return new Range[]{new Range(this)};
     }
 
     public boolean isIntersected(Range other) {
@@ -66,15 +69,7 @@ public class Range {
         return from;
     }
 
-    public void setFrom(double from) {
-        this.from = from;
-    }
-
     public double getTo() {
         return to;
-    }
-
-    public void setTo(double to) {
-        this.to = to;
     }
 }
