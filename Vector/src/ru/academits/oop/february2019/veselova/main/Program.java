ca03858c4450ba287b650805803929104779aac5
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-03-13 16:16:58 +0700

    Added functionality extension in Vector task.

--- Vector/src/ru/academits/oop/february2019/veselova/main/Program.java
+++ Vector/src/ru/academits/oop/february2019/veselova/main/Program.java
@@ -1,7 +1,35 @@
 package ru.academits.oop.february2019.veselova.main;
 
+import ru.academits.oop.february2019.veselova.vector.Vector;
+
 public class Program {
     public static void main(String[] args) {
+        Vector v1 = new Vector(new double[]{1, 2, 3, 4, 5});
 
+        Vector v2 = new Vector(3, new double[]{1, 2, 3, 4});
+
+        Vector v3 = new Vector(v2);
+
+        Vector v4 = new Vector(4);
+
+        System.out.printf("%s + %s =%s\n\n", v1, v2, Vector.add(v1, v2));
+
+        System.out.printf("%s * %s =%f\n\n", v1, v2, Vector.scalarProduct(v1, v2));
+
+        v1.subtractVector(v2);
+        System.out.printf("v1 - v2 = %s\n\n", v1);
+
+        v2.multiplyByScalar(10);
+        System.out.printf("v2 * 10 = %s\n\n", v2);
+
+        v3.revertVector();
+        System.out.printf("reverted v3  = %s\n\n", v3);
+
+        v4.setComponentByIndex(0, 0);
+        v4.setComponentByIndex(1, -1);
+        v4.setComponentByIndex(2, -2);
+        v4.setComponentByIndex(3, -3);
+
+        System.out.printf("v4  = %s\n\n", v4);
     }
 }
--- Vector/src/ru/academits/oop/february2019/veselova/vector/Vector.java
+++ Vector/src/ru/academits/oop/february2019/veselova/vector/Vector.java
@@ -7,7 +7,6 @@ public class Vector {
         if (n < 0) {
             throw new IllegalArgumentException("Error. Negative dimension of a vector.");
         }
-
         vector = new double[n];
     }
 
@@ -17,9 +16,8 @@ public class Vector {
         }
 
         vector = new double[other.getSize()];
-        double[] otherArray = other.getVector();
-        for (int i = 0; i < vector.length; i++) {
-            vector[i] = otherArray[i];
+        for (int i = 0; i < getSize(); i++) {
+            vector[i] = other.vector[i];
         }
     }
 
@@ -29,13 +27,13 @@ public class Vector {
         }
 
         vector = new double[array.length];
-        for (int i = 0; i < vector.length; i++) {
+        for (int i = 0; i < getSize(); i++) {
             vector[i] = array[i];
         }
     }
 
 
-    Vector(int n, double[] array) {
+    public Vector(int n, double[] array) {
         if (n < 0) {
             throw new IllegalArgumentException("Error. Negative dimension of a vector.");
         }
@@ -44,13 +42,11 @@ public class Vector {
         }
 
         vector = new double[n];
-        for (int i = 0; i < array.length; i++) {
+
+        int m = Math.min(n, array.length);
+        for (int i = 0; i < m; i++) {
             vector[i] = array[i];
         }
-    }
-
-    public double[] getVector() {
-        return vector;
     }
 
     public int getSize() {
@@ -61,12 +57,112 @@ public class Vector {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{ ");
-        for (int i = 0; i < vector.length; i++) {
+        for (int i = 0; i < getSize(); i++) {
             sb.append(vector[i] + ", ");
         }
-        sb.delete(sb.length() - 2, 2);
+        sb.setLength(sb.length() - 2);
         sb.append(" }");
 
         return sb.toString();
+    }
+
+    public void addVector(Vector other) {
+        int m = Math.min(getSize(), other.getSize());
+
+        for (int i = 0; i < m; i++) {
+            vector[i] += other.vector[i];
+        }
+    }
+
+    public void subtractVector(Vector other) {
+        int m = Math.min(getSize(), other.getSize());
+
+        for (int i = 0; i < m; i++) {
+            vector[i] -= other.vector[i];
+        }
+    }
+
+    public void multiplyByScalar(double scalarValue) {
+        for (int i = 0; i < getSize(); i++) {
+            vector[i] *= scalarValue;
+        }
+    }
+
+    public void revertVector() {
+        multiplyByScalar(-1);
+    }
+
+    public double getComponentByIndex(int index) {
+        if (index >= getSize()) {
+            throw new IndexOutOfBoundsException("Error. Out of bounds array.");
+        }
+        return vector[index];
+    }
+
+    public void setComponentByIndex(int index, double value) {
+        if (index >= getSize()) {
+            throw new IndexOutOfBoundsException("Error. Out of bounds array.");
+        }
+        vector[index] = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (o == null || o.getClass() != this.getClass()) return false;
+        Vector other = (Vector) o;
+        if (getSize() != other.getSize()) return false;
+
+        for (int i = 0; i < getSize(); i++) {
+            if (vector[i] != other.vector[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (int i = 0; i < getSize(); i++) {
+            hash += Double.hashCode(vector[i]);
+        }
+        return hash;
+    }
+
+    public static Vector add(Vector first, Vector second) {
+        Vector result;
+        if (first.getSize() >= second.getSize()) {
+            result = new Vector(first);
+            result.addVector(second);
+        } else {
+            result = new Vector(second);
+            result.addVector(first);
+        }
+
+        return result;
+    }
+
+    //TODO сделать адекватно работающее вычитание static и nonstatic
+    public static Vector subtract(Vector first, Vector second) {
+
+        Vector result;
+        if (first.getSize() >= second.getSize()) {
+            result = new Vector(first);
+            result.subtractVector(second);
+        } else {
+            result = new Vector(second);
+            result.subtractVector(first);
+        }
+        return result;
+    }
+
+    public static double scalarProduct(Vector first, Vector second) {
+        double calculateScalarProduct = 0;
+        int m = Math.min(first.getSize(), second.getSize());
+        for (int i = 0; i < m; i++) {
+            calculateScalarProduct += first.vector[i] * second.vector[i];
+        }
+        return calculateScalarProduct;
     }
 }
