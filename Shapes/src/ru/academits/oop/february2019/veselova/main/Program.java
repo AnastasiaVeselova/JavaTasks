ad11c5157ad0bc48866c5e615bc8c7ddeb28347c
Anastasia Veselova <anastasia.v3selova@yandex.ru>
2019-03-14 13:58:50 +0700

    Removed unnecessary imports.

--- Shapes/src/ru/academits/oop/february2019/veselova/main/Program.java
+++ Shapes/src/ru/academits/oop/february2019/veselova/main/Program.java
@@ -5,7 +5,6 @@ import ru.academits.oop.february2019.veselova.comparators.ShapePerimeterComparat
 import ru.academits.oop.february2019.veselova.shapes.*;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,7 +12,7 @@ public class Program {
                 new Rectangle(5, 10),
                 new Square(20.5),
                 new Square(15),
-                new Triangle(1, 2, 5, 8, -4, -7),
+                new Triangle(-1, 2, 5, 8, -4, -7),
 
                 new Rectangle(1, 3),
                 new Circle(31),
